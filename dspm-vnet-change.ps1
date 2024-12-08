aaf19b69591d9fa83c5a35a73a603237a3eb9006
Erick Moore <erick@prosimo.io>
2024-12-08 16:42:39 -0500

    feat: added logging and progress bar

--- dspm-vnet-change.ps1
+++ dspm-vnet-change.ps1
@@ -1,21 +1,47 @@
-$digTagName              = 'dig-security'
-$defaultDigAddressPrefix = '10.0.0.0/16'
-$newDigSubnetPrefix      = '10.61.8.0/24'
+$tagName      = 'dig-security'
+$currentCIDR  = '10.0.0.0/24'
+$newCIDR      = '10.50.0.0/24'
 
 $allVnets = Get-AzVirtualNetwork
 foreach ($vnet in $allVnets) {
+    $counter++
+    $percentComplete = ($counter / $totalVnets) * 100
+    Write-Progress -Activity "Processing VNets" -Status "Processing VNet $counter of $totalVnets" -PercentComplete $percentComplete
+    
     if (-not $vnet.Tag) { continue }
-    if ($vnet.Tag.ContainsKey($digTagName)) {
+    if ($vnet.Tag.ContainsKey($tagName)) {
+        $subnetName = "$($tagName)-$($vnet.Location)"
 
-        $digSubnetName = "$($digTagName)-$($vnet.Location)"
+        try {
+            # Backup VNet as JSON file
+            backup-vnet -vnet $vnet
 
-        $vnet.AddressSpace.AddressPrefixes.Add($newDigSubnetPrefix)
-        Set-AzVirtualNetwork -VirtualNetwork $vnet
+            # Remove existing subnet
+            Remove-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
 
-        Remove-AzVirtualNetworkSubnetConfig -Name $digSubnetName -VirtualNetwork $vnet
-        Add-AzVirtualNetworkSubnetConfig -Name $digSubnetName -AddressPrefix $newDigSubnetPrefix -VirtualNetwork $vnet
+            # Add new CIDR and remove old CIDR
+            $vnet.AddressSpace.AddressPrefixes.Add($newCIDR)
+            $vnet.AddressSpace.AddressPrefixes.Remove($currentCIDR)
 
-        $vnet.AddressSpace.AddressPrefixes.Remove($defaultDigAddressPrefix)
-        Set-AzVirtualNetwork -VirtualNetwork $vnet
+            # Update VNet with new values
+            Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $newCIDR -VirtualNetwork $vnet
+            Set-AzVirtualNetwork -VirtualNetwork $vnet
+        }
+        catch {
+            Write-Error "Failed to modify VNet $($vnet.Name): $_"
+        }
+        finally {}
+
+
     }
+}
+
+function backup-vnet {
+    param (
+        [Parameter(Mandatory = $true)]
+        [Microsoft.Azure.Commands.Network.Models.PSVirtualNetwork] $vnet
+    )
+    $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+    $filename = "vnet-modification-before-$timestamp.json"
+    $vnet | ConvertTo-Json -Depth 10 | Out-File -FilePath $filename
 }
