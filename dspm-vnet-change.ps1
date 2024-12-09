58014e21146db3b39c380db33c4c9293db538081
Erick Moore <erick.moore@gmail.com>
2024-12-09 11:57:30 -0500

    added full removal

--- dspm-vnet-change.ps1
+++ dspm-vnet-change.ps1
@@ -1,21 +1,16 @@
 $tagName      = 'dig-security'
-$currentCIDR  = '10.0.0.0/16'
 $newCIDR      = '10.61.8.0/24'
 $allVnets     = Get-AzVirtualNetwork
 $vnetCount    = $allVnets.Count
 
-try {
-    $currentAddress = Read-Host -Prompt "Enter the current CIDR for the VNet (default: $currentCIDR)"
-    $newAddress     = Read-Host -Prompt "Enter the new CIDR for the VNet (default: $newCIDR)"
-
-    if (-not $currentAddress) { $currentAddress = $currentCIDR }
-    if (-not $newAddress) { $newAddress = $newCIDR }
+function Test-CIDR {
+    param (
+        [string]$cidr
+    )
+    $regex = '^([0-9]{1,3}\.){3}[0-9]{1,3}\/([0-9]|[1-2][0-9]|3[0-2])$'
+    return $cidr -match $regex
 }
-catch {
-    Write-Host "Error: $($_.Exception.Message)"
-}
-
-function backup-vnet {
+function Backup-VNet {
     param (
         [Parameter(Mandatory = $true)]
         [Microsoft.Azure.Commands.Network.Models.PSVirtualNetwork] $vnet
@@ -31,12 +26,28 @@ function backup-vnet {
     #     -Force 
 }
 
+do {
+    $newAddress = Read-Host -Prompt "Enter the new CIDR for the VNet (default: $newCIDR)"
+
+    # Use default value if none provided
+    if (-not $newAddress) { $newAddress = $newCIDR }
+
+    # Validate provided CIDR is in valid format
+    $isValid = Test-CIDR -cidr $newAddress
+
+    if (-not $isValid) {
+        Write-Host ""
+        Write-Host "Invalid CIDR format. Please enter a valid CIDR notation (e.g., 10.1.0.0/24)." -BackgroundColor DarkRed -ForegroundColor White
+    }
+}
+until ($isValid)
+
 foreach ($vnet in $allVnets) {
-    
     $counter++
     $percentComplete = ($counter / $vnetCount) * 100
     Write-Progress -Activity "Processing VNets" -Status "Processing VNet $counter of $vnetCount" -PercentComplete $percentComplete
 
+    # If no tag on VNet skip it and wait 1 second so progress bar is visible
     if (-not $vnet.Tag) { Start-Sleep -Seconds 1; continue }
     
     if ($vnet.Tag.ContainsKey($tagName)) {
@@ -44,17 +55,25 @@ foreach ($vnet in $allVnets) {
 
         try {
             # Backup VNet as JSON file
-            backup-vnet -vnet $vnet
+            Backup-VNet -vnet $vnet
 
-            # Remove existing subnet
-            Remove-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet > $null
-
-            # Add new CIDR and remove old CIDR
+            # Remove all existing address space
+            foreach ($address in $($vnet.AddressSpace.AddressPrefixes)) {
+                $vnet.AddressSpace.AddressPrefixes.Remove($address) > $null
+            }
+            
+            # Add new address space
             $vnet.AddressSpace.AddressPrefixes.Add($newAddress)
-            $vnet.AddressSpace.AddressPrefixes.Remove($currentAddress)
 
-            # Update VNet with new values
+            # Remove all subnets
+            foreach ($subnet in $($vnet.Subnets)) {
+                $vnet.Subnets.Remove($subnet) > $null
+            }
+
+            # Add new subnet
             Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $newAddress -VirtualNetwork $vnet > $null
+
+            # Update VNet config
             Set-AzVirtualNetwork -VirtualNetwork $vnet > $null
         }
         catch {
